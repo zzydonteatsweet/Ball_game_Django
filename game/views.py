7825a00cf096d609dd4faca9b54f8ad33e012b2b
jack old <zzyfocusonlife@foxmail.com>
2022-05-12 13:04:25 +0800

    Project created

--- game/views.py
+++ game/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def index(request):
+    line1 = '<h1 style="text-align : center">"术士之战"</h1>'
+    return HttpResponse("我的第一个网页" + line1)
