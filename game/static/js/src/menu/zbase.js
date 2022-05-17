0bed120127cc77e28384266446f82f7ed199f99b
jack old <zzyfocusonlife@foxmail.com>
2022-05-17 17:44:52 +0800

    menu constructed

--- game/static/js/src/menu/zbase.js
+++ game/static/js/src/menu/zbase.js
@@ -54,18 +54,4 @@ class AcGameMenu {
             this.$menu.hide() ;
         }
     
-}class ACgame {
-    constructor(id) {
-        // console.log("Create ACgame") ;
-        this.id = id ;
-        this.$ac_game_1 = $('#' + id) ;
-        this.menu = new AcGameMenu(this) ; 
-        // this.playgroud = new AcGamePlayground(this) ;
-        this.start() ;
-    }
-
-    start() {
-
-    }
-    
 }
--- game/static/js/src/zbase.js
+++ game/static/js/src/zbase.js
@@ -1,5 +1,15 @@
 class ACgame {
     constructor(id) {
-        
+        // console.log("Create ACgame") ;
+        this.id = id ;
+        this.$ac_game_1 = $('#' + id) ;
+        this.menu = new AcGameMenu(this) ; 
+        // this.playgroud = new AcGamePlayground(this) ;
+        this.start() ;
     }
+
+    start() {
+
+    }
+    
 }
